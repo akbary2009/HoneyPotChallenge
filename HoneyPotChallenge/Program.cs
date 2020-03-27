35b354fe18d7f318fe4cbfad1f3ba847bbae195f
user <user@localhost>
2020-03-27 19:30:12 +0430

    Add project files.

---
+++ HoneyPotChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HoneyPotChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
