e5a6eaff3a775d0977957fe76fcc3612d9ae9a0f
user <user@localhost>
2020-03-27 19:38:03 +0430

    Adding `MinCans` challenge's solution with sample input.

---
+++ HoneyPotChallenge/Challenge.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HoneyPotChallenge
+{
+    public class Challenge
+    {
+        public static int MinCans(List<int> used, List<int> total)
+        {
+            if (total.Count != used.Count)
+                throw new ArgumentException($"{nameof(total)} number of cans must be the same as {nameof(used)} number of cans.", nameof(total));
+            if (!total.Any() || !used.Any())
+                return default(int);
+
+            //var remainingSpaces = total.Zip(used, (t, u) => t - u);
+            //if (remainingSpaces.Any(rs => rs < 0))
+            //    throw new ArgumentException($"{nameof(used)} amount cannot be more than total amount", nameof(used));
+
+            var totalRequiredSpace = used.Sum();
+            var totalCansNeeded = 0;
+
+            foreach (var availableSpace in total.OrderByDescending(space => space))
+            {
+                totalRequiredSpace -= availableSpace;
+                totalCansNeeded++;
+                if (totalRequiredSpace <= 0)
+                    break;
+            }
+            return totalCansNeeded;
+        }
+    }
+}
